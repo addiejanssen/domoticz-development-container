cde0dd861cb1e3c9d761c32d3bed048514ba77d2
Addie Janssen <me@addiejanssen.com>
2026-09-13 17:54:41 +0200

    Use correct OS and MACH in download

--- install-domoticz.sh
+++ install-domoticz.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# Function to convert a string to lowercase
+lowercase(){
+    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
+}
+
+DOWNLOAD_CHANNEL="release"
+OS=`lowercase \`uname -s\``
+MACH=`uname -m`
+
 # Install Domoticz in the $DOMOTICZ_DIR directory
-wget -q -O /tmp/domoticz_release.tgz "https://www.domoticz.com/download.php?channel=release&type=release&system=linux&machine=x86_64"
+wget -q -O /tmp/domoticz_release.tgz "https://www.domoticz.com/download.php?channel=${DOWNLOAD_CHANNEL}&type=release&system=${OS}&machine=${MACH}"
+
 mkdir -p $DOMOTICZ_DIR
 tar -xzf /tmp/domoticz_release.tgz -C $DOMOTICZ_DIR
 rm /tmp/domoticz_release.tgz
